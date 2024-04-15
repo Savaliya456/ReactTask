bab5acf064e61c4efafb69e435ded844149295e4
Savaliya456 <hepin3584@gmail.com>
2024-04-15 18:33:08 +0530

    Complete

--- day-1/src/App.js
+++ day-1/src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import About from './About';
+import Person from './Person';
+import Success from './Success';
+import Demo from './Demo';
+import Footer from './Footer';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Header/>
+    <About/>
+    <Person/>
+    <Success/>
+    <Demo/>
+    <Footer/>
     </div>
   );
 }
