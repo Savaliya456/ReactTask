aa8b94e26e9c971600698254ed660dbeda667f05
Savaliya456 <hepin3584@gmail.com>
2024-04-30 18:41:37 +0530

    Complete

---
+++ event-condition/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import EventCondition from './EventCondition';
+
+function App() {
+  return (
+    <div className="App">
+      <EventCondition/>
+    </div>
+  );
+}
+
+export default App;
+
