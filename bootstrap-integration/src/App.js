49064cd392ff8a0fceabd84590aea91b1ee2bd8b
Savaliya456 <hepin3584@gmail.com>
2024-05-01 17:28:24 +0530

    Complete

--- bootstrap-integration/src/App.js
+++ bootstrap-integration/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import TopBar from './Components/TopBar';
+import Header from './Components/Header';
+import Section01 from './Components/Section01';
+import Section02 from './Components/Section02';
+import Section03 from './Components/Section03';
+import Section04 from './Components/Section04';
+import Section05 from './Components/Section05';
+import Section06 from './Components/Section06';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TopBar/>
+      <Header/>
+      <Section01/>
+      <Section02/>
+      <Section03/>
+      <Section04/>
+      <Section05/>
+      <Section06/>
+      <Footer/>
     </div>
   );
 }
